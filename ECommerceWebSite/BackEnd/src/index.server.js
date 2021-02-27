7dad1b099e440d51679d0eea6c043d9944d6781f
noman <nomanfcb@gmail.com>
2021-02-27 17:43:37 +0600

    initial add v0.1

--- ECommerceWebSite/BackEnd/src/index.server.js
+++ ECommerceWebSite/BackEnd/src/index.server.js
@@ -1,55 +1,46 @@
-const express = require("express");
-const env = require("dotenv");
+//All Imports here
+const express = require('express');
+const env = require('dotenv');
 const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-//routes
-const authRoutes = require("./routes/auth");
-const adminRoutes = require("./routes/admin/auth");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const cartRoutes = require("./routes/cart");
-const initialDataRoutes = require("./routes/admin/initialData");
-const pageRoutes = require("./routes/admin/page");
-const addressRoutes = require("./routes/address");
-const orderRoutes = require("./routes/order");
-const adminOrderRoute = require("./routes/admin/order.routes");
+//Routes
+const userRoutes = require('./routes/user')
 
-//environment variable or you can say constants
+//code body starts here
 env.config();
-
-// mongodb connection
-//mongodb+srv://root:<password>@cluster0.8pl1w.mongodb.net/<dbname>?retryWrites=true&w=majority
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.8pl1w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
-  .then(() => {
-    console.log("Database connected");
-  });
-
-app.use(cors());
 app.use(express.json());
-app.use("/public", express.static(path.join(__dirname, "uploads")));
-app.use("/api", authRoutes);
-app.use("/api", adminRoutes);
-app.use("/api", categoryRoutes);
-app.use("/api", productRoutes);
-app.use("/api", cartRoutes);
-app.use("/api", initialDataRoutes);
-app.use("/api", pageRoutes);
-app.use("/api", addressRoutes);
-app.use("/api", orderRoutes);
-app.use("/api", adminOrderRoute);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+//MongoDB Database Settings
+const PASS = process.env.MONGODB_PASSWORD;
+const USER = process.env.MONGODB_USER_ID;
+const DATABASE = process.env.MONGODB_DATABASE;
+mongoose.connect(
+   `mongodb+srv://${USER}:${PASS}@cluster0.f6ijt.mongodb.net/${DATABASE}?retryWrites=true&w=majority`,
+   {
+       useFindAndModify: true,
+       useUnifiedTopology: true, 
+       connectTimeoutMS: 10000,
+       poolSize: 10,
+       writeConcern: {
+           j: true
+       }
+   }
+).then(()=>{
+    console.log('Server Connected To Database');
+})
+
+//MiddleWares APIS
+app.use('/api',userRoutes);
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+    console.log(`Server is Running on port: ${PORT}`)
+})
---
+++ ECommerceWebSite/BackEnd/src/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+
+router.post('/signup',(req,res,next)=>{
+
+})
+
+router.post('/signin',(req,res,next)=>{
+
+})
+
+module.exports = router;
